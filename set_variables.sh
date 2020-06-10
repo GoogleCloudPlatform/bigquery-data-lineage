09a04df7e11255291a72e263f683d0527dd32c5a
Anant Damle <anant.damle@gmail.com>
2020-06-10 13:33:26 +0000

    Update instructions with latest code. Update Surefire plugin version

--- set_variables.sh
+++ set_variables.sh
@@ -13,14 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export PROJECT_ID=$(gcloud config get-value project)
-export REGION_ID=us-central1
-export AUDIT_LOGS_PUBSUB_TOPIC=bq-audit-logs
-export LINEAGE_OUTPUT_PUBSUB_TOPIC=composite-lineage
-export DATASET_ID=audit_dataset
-export LINEAGE_TABLE_ID=Lineage
-export TEMP_GCS_BUCKET=bigquery-lineage-demo-temp
-export LINEAGE_TAG_TEMPLATE_NAME=data_lineage_tag
+PROJECT_ID="$(gcloud config get-value project)"
+export PROJECT_ID
+export REGION_ID="us-central1"
+export AUDIT_LOGS_PUBSUB_TOPIC="bq-audit-logs"
+export LOG_SINK_ID="bq_audit_logs_to_pubsub"
+export LINEAGE_OUTPUT_PUBSUB_TOPIC="composite-lineage"
+export BIGQUERY_REGION="us"
+export DATASET_ID="audit_dataset"
+export LINEAGE_TABLE_ID="Lineage"
+export TEMP_GCS_BUCKET="$(whoami)-${PROJECT_ID}-dataflow-temp"
+export LINEAGE_TAG_TEMPLATE_NAME="data_lineage_tag"
 export LINEAGE_TAG_TEMPLATE_ID="projects/$PROJECT_ID/locations/$REGION_ID/tagTemplates/$LINEAGE_TAG_TEMPLATE_NAME"
 export EXTRACTION_MAIN_CLASS="com.google.cloud.solutions.datalineage.LineageExtractionPipeline"
 export PROPAGATION_MAIN_CLASS="com.google.cloud.solutions.datalineage.PolicyPropagationPipeline"
